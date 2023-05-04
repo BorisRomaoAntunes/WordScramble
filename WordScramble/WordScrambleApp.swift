fd4614e2f57497348769a3472db0534ae25a7dda
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-04 18:59:18 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Boris R on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
